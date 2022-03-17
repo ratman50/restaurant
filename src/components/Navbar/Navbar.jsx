2c877bd3f1cc97071a8dff75f65def15e48a181c
pape mactar ndiaye <ratma96@gmail.com>
2022-03-17 12:44:37 +0000

    finition html navabr

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,8 +11,16 @@ import images from '../../constants/images';
 const Navbar = () => (
   <nav className='app__navbar'> 
     <div className='appp_navbar-logo'>
-      
-      </div>  
+      <img src={images.gericht} alt='app log'/>
+    </div> 
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'><a href='#home'>Home</a></li> 
+      <li className='p__opensans'><a href='#about'>About</a></li> 
+      <li className='p__opensans'><a href='#menu'>Menu</a></li> 
+      <li className='p__opensans'><a href='#awards'>Awards</a></li> 
+      <li className='p__opensans'><a href='#contact'>Contacts</a></li> 
+
+    </ul>
 
   </nav>
 );
